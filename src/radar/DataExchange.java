bce7569a3871beb95e660d999cfec0c2053ed99e
Roman Šilhan <xsilhan1@fi.muni.cz>
2018-06-10 19:24:50 +0200

    Javadoc completed

--- src/radar/DataExchange.java
+++ src/radar/DataExchange.java
@@ -5,104 +5,63 @@ import lejos.hardware.motor.NXTRegulatedMotor;
  * @author Gallo, Silhan
  * @version 2018
  * 
- * Class which is used to save needed data.
+ * Utility Class to exchange information between threads.
  */
 public class DataExchange {
 	
 	Status status = null;
-	
 	// Toto je len testovacia premenna, ktora sa nastavi na true
 	// vtedy ak sa nejaky objekt nachadza 30-50cm vzdialenosti
 	// Vo finalnom produkte by sa nemala nachadzat
 	private boolean stopRadar;
-	
-	// Premenna, ktora da triede Wanad vediet, ze ma vypocitat uhol theta
-	// a nastavit tento uhol na motore
-	private boolean aimTarget;
-	
-	// Premenna, ktora da triede FireUnit vediet, ze moze strielat
-	// blizsie nespecifikovane -- bud sa nastavi v okamih ked su
-	// motory v spravnej pozicii alebo predtym, s tym, ze nevystreli
-	// skor/neskor 
-	private boolean fireAtWill;
-	
-	// Vzdialenost merana IR senzorom
-	private int distanceToTarget;
-	
+
+
 	// Uhol theta (v stupnoch), ktory sa ma nastavit na motore B
 	private int weaponAngle;
 	
 	// Vzdialenost objektu od dela
 	private int x;
 	
-	// Relativna vertikalna vzdialenost voci delu (delo je v [0,0])
-	private int y;
-	
-	// Pozice motoru kdyz objevil cil
-	private int positionOfScanMotorWhenFindTarget = Integer.MIN_VALUE;
-	
 	private NXTRegulatedMotor scannerMotorA;
 	
+	/**
+	 * Get scanner motor
+	 * @return NXTRegulatedMotor Scanner motor
+	 */
 	public NXTRegulatedMotor getScannerMotorA() {
 		return scannerMotorA;
 	}
 
 
-
+	/**
+	 * Set scanner motor
+	 * @param nxtRegulatedMotor NXTRegulatedMotor motor for moving with the scanner
+	 */
 	public void setScannerMotorA(NXTRegulatedMotor nxtRegulatedMotor) {
 		this.scannerMotorA = nxtRegulatedMotor;
 	}
 
 
-
+	/**
+	 * Constructor of DataExchange
+	 */
 	public DataExchange() {
 		stopRadar = false;
-		aimTarget = false;
-		fireAtWill = false;
 		status = Status.MEASURE;
 	}
 	
-	
-	
-	public boolean isStopRadar() {
-		return stopRadar;
-	}
-
-
-
-	public void setStopRadar(boolean stopRadar) {
-		this.stopRadar = stopRadar;
-	}
-
-
-
-	public boolean isAimTarget() {
-		return aimTarget;
-	}
-
-
-
-	public void setAimTarget(boolean aimTarget) {
-		this.aimTarget = aimTarget;
-	}
-
-
-
-	public boolean isFireAtWill() {
-		return fireAtWill;
-	}
-
-
-	public void setFireAtWill(boolean fireAtWill) {
-		this.fireAtWill = fireAtWill;
-	}
-
-
-
+	/**
+	 * Get the distance to target
+	 * @return x int distance to target
+	 */
 	public int getX() {
 		return x;
 	}
-	
+
+	/**
+	 * Set the distance to target
+	 * @param x int distance to target
+	 */
 	public void setX(int x) {
 		this.x = x;
 	}
@@ -116,31 +75,11 @@ public class DataExchange {
 		stopRadar = val;
 	}
 	
-	public int getAngle() {
-		return weaponAngle;
-	}
 	
-	public void setAngle(int angle) {
-		weaponAngle = angle;
-	}
 
 
-
-	public int getPosOfScanWhenFindTarget() {
-		return positionOfScanMotorWhenFindTarget;
-	}
-
-
-
-	public void setPosOfScanWhenFindTarget(int positionOfScanMotorWhenFindTarget) {
-		this.positionOfScanMotorWhenFindTarget = positionOfScanMotorWhenFindTarget;
-	}
 	
-	public void resetMeasuredValues() {
-		x = 0;
-		y = 0;
-		distanceToTarget = -1;
-		setAimTarget(false);
-	}
+
+	
 
 }
